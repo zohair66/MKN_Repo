7157b730e347776111e1cebd1bb9ce4c9f3bf19d
msarraf <m.sheikhsarraf@gmail.com>
2015-02-14 15:45:44 +0330

    ویرایش

--- home.php
+++ home.php
@@ -100,7 +100,7 @@ require_once("includes/includes.php");
     <img src="<?php echo $homecontent1[0]->picturelink  ?>">
 </div>
 <div id="homecontent1text" class="homecontent1"><?php echo $homecontent1[0]->content ?></div>
-<a href="<?php echo $homecontents[0]->link ?>" id="homecontent1more" class="homecontentmore">اطلاعات بیشتر</a>
+<a href="#" id="homecontent1more" class="homecontentmore" onclick="moreinfo1()">اطلاعات بیشتر</a>
 
 </div>
 <div id="homecontent2" class="homecontent">
@@ -164,13 +164,28 @@ require_once("includes/includes.php");
         <?php echo $homecontent4[0]->content ?>
     </div>
 
-    <a href="<?php echo $homecontents[3]->link ?>" id="homecontent4more" class="homecontentmore">اطلاعات بیشتر</a>
+    <a href="<?php echo $homecontents[3]->link ?>" id="homecontent4more" class="homecontentmore" onclick="moreinfo4()">اطلاعات بیشتر</a>
 
 </div>
 </div>
+<div id="homemore1" class="homemore"><p class="homemoretitle">خوش آمدید</p><p class="homemoretext">امروزه دنیای مجازی در معرفی کالا و یا خدمات نقش بسزایی دارد.  فناوری اطلاعات در زمینه¬های بسیاری ازجمله اطلاع رسانی، آموزش، خدمات رسانی و فروش کاربرد دارد. استفاده از این قابلیت می¬تواند به ارتقای کسب و کار به ویژه در دنیای امروز کمک کند. علاوه بر این، اقبال عمومی به استفاده از فناوری اطلاعات در عرصه¬های مختلف رو به افزایش است و این امکانات به رقابت پذیری مشاغل نیز کمک می¬کند. از این رو گروه مهندسان فناوری ماکان برای هر یک از زمینه های کاری از جمله شرکت ها و موسسات آموزشی، سایت های  اطلاع رسانی کالا و  یا خدمات، بسته به نیاز به آماده سازی و طراحی پرتال های اینترنتی با کمترین هزینه اقدام نموده¬اند. به دلیل گستردگی فضای مجازی، مشخص است که فقط بهترین و با کیفیت ترین کارها توانایی رقابت و باقی ماندن در این حوزه را دارند، لذا در مجموعه ماکان سعی بر ارائه خدمت با بهترین کیفیت است. 
+مشخصه دیگر فضای مجازی و علوم مربوط به فناوری اطلاعات، تغییرات گسترده در مدت زمانی اندک است. این امر  باعث از رده خارج شدن سایت¬هایی خواهد شد که بر این موج تغییرات سوار نیستند. یکی از  مهمترین ویژگی¬های گروه ماکان استفاده از جدیدترین امکانات و اطلاع رسانی به مشتریان جهت نصب و راه-اندازی افزونه¬های جدید است. مساله دیگری که در راه اندازی و نگهداری از سایت¬ها بسیار اهمیت دارد پشتیبانی و به روز بودن مطالب است. این امر به مهمترین مشکل بسیاری از سایت¬های طراحی شده تا کنون تبدیل شده ¬است. برای رفع این مشکل در مجموعه ماکان، تیم مخصوص و متعهدی جهت به¬روز نگه داشتن اطلاعات فعالیت می¬کنند. شاخص بودن سایت در موتورهای جستجو از دیگر مواردی است که مهندسان گروه ماکان برای افزایش سطح دسترسی به سایت¬های طراحی شده مورد توجه قرار می¬دهند. 
+بهره گیری از نیروهای متعهد و اولولیت دادن به کیفیت کارها، مهمترین دغدغه مجموعه ماکان است.
+</p></div>
+<div id="homemore4" class="homemore"></div>
 </div>
+<div  id="footerdiv">
 <p  class="footer" id="footername">تمام حقوق مادی و معنوی این سایت متعلق به دفتر خدمات طراحی وب سایت ماکان است</p>
 <p class="footer" id="footeraddress"><br />آدرس:اصفهان خیابان میرداماد بعد از چهارراه زاهد طبقه فوقانی دفتر اسناد رسمی 410 </p>
 <p class="footer" id="footertelefon">تلفن:32352016-031 تلفکس:32362237-031</p>
+</div>
+<script>
+function moreinfo1(){
+    content = document.getElementById("content");
+    content.style.transform = "translate(1500px,0px)";
+    homemore1=document.getElementById("homemore1");
+    homemore1.style.transform = "translate(0px,0px)";
+}
+</script>
 </body>
 </html>
